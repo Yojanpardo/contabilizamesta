7f4f67573b0a6dc9c98679b6b893bd1147bdaca0
Yojan Pardo <yojan.pardo@gmail.com>
2016-06-02 16:31:02 -0500

    login bonito

---
+++ javascript/login.js
@@ -0,0 +1,28 @@
+
+
+function iniciar() {
+	usuario=document.getElementById("usuario");
+	pass=document.getElementById("pass");
+	usuario.addEventListener("input",validacion,false);
+	pass.addEventListener("input",validacion,false);
+	validacion();
+
+}
+
+function validacion() {
+	if(usuario.value==""){
+		usuario.setCustomValidity("Ingresa un usuario");
+		usuario.style.background="#FFDDDD";
+	}else{
+		usuario.setCustomValidity("");
+		usuario.style.background="#FFFFFF";
+	}
+	if(pass.value==""){
+		pass.setCustomValidity("Ingresa una contraseña");
+		pass.style.background="#FFDDDD";
+	}else{
+		pass.setCustomValidity("");
+		pass.style.background="#FFFFFF";
+		
+	}
+}
